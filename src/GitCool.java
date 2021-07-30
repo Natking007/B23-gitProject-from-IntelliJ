6d9c7d4534d8373e32a1b676282430ccb99fdf58
Natking007 <77073455+Natking007@users.noreply.github.com>
2021-07-30 08:34:33 -0400

    Since I have shared the project, the next step is to PUSH

--- src/GitCool.java
+++ src/GitCool.java
@@ -5,4 +5,12 @@ public class GitCool {
 
     // Just shared project on Github from IntelliJ
     // make sure to click apply and save
+
+    //we were able to successfully shared project under
+    //out github account
+    //share project in github happen only one time per project
+    // it combines the 3 action below
+    // create remote repository under your account
+    // create the link between local rep to remote repo
+    // push local commit to remote repo
 }
