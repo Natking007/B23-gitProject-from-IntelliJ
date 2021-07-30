fef50e4719651efe25fd8582f9ec8d038f65acf5
Natking007 <77073455+Natking007@users.noreply.github.com>
2021-07-30 08:30:33 -0400

    added github account

--- src/GitCool.java
+++ src/GitCool.java
@@ -2,4 +2,7 @@ public class GitCool {
     //B23 is learning Git
     // B23 is making some sense now
     //B23 has something more to say
+
+    // Just shared project on Github from IntelliJ
+    // make sure to click apply and save
 }
