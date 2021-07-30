6cc8360fb1b0c20982be961e6b812324bd8782fe
Natking007 <77073455+Natking007@users.noreply.github.com>
2021-07-30 10:50:39 -0400

    added comment collection2 does not know about, edited typo

--- src/DayFour/Conflict.java
+++ src/DayFour/Conflict.java
@@ -2,6 +2,6 @@ package DayFour;
 
 public class Conflict {
     //this is master branch content
-    // This is the chnage collection2 branch does
+    // This is the change collection2 branch does
     // not know about
 }
